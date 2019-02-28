83eafe3848135a012385191186982005c33b3f34
Pandelis Zembashis <info@pandelis.me>
2019-02-28 12:33:04 +0000

    ADD: hover over readme

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-
+import 'babel-polyfill'
 const nameNode = document.querySelector('.blade--left > h1')
 const subtitleNode = document.querySelector('.blade--right > h1')
 const profileHeaderText = document.querySelector('#profile > h1')
 const projectsHeaderText = document.querySelector('#projects > h1')
 const visitorNameNodes = document.querySelectorAll(".visitorName")
 const getInTouch = document.querySelector("#social > h1")
+const gridItems = document.querySelectorAll('.grid__item.readme')
+const modalTemplate = document.querySelector('.modalTemplate')
+const modalCode = document.querySelector('.modalTemplate > div.markdown')
 
 const mastheadScroller = function(scrollPosition) {
 
@@ -19,7 +22,7 @@ const mastheadScroller = function(scrollPosition) {
 
 let ticking = false;
 window.addEventListener('scroll', function(e) {
-    last_known_scroll_position = window.scrollY;
+    let last_known_scroll_position = window.scrollY;
 
     if (!ticking) {
       window.requestAnimationFrame(function() {
@@ -54,5 +57,59 @@ visitorNameNodes.forEach(div => {
     div.addEventListener("cut", visitorName);
     div.addEventListener("delete", visitorName);
     div.addEventListener("mouseup", visitorName);
+})
+
+
+const readmes = {}
+let reverse = false
+gridItems.forEach(div => {
+
+  div.addEventListener('mousemove', (e) => {
+    let mouseTick = false;
+    if (!mouseTick) {
+      window.requestAnimationFrame(function() {
+
+        let left = e.pageX + 3
+
+        if (left >= (window.innerWidth - 400) || reverse === true) {
+          left = left - 403
+          reverse = true
+        }
+
+        if (left <= 400 && reverse === true) {
+          left = e.pageX + 3
+          reverse = false
+        }
+
+        modalTemplate.style = `left: ${left}px; top: ${e.pageY+10}px`
+
+      });
+
+    }
+
+    mouseTick = true;
+
+  })
+
+  div.addEventListener('mouseover', async (e) => {
+
+    if (e.target.dataset.readme != 'underfined' && e.target.dataset.readme != '' && typeof e.target.dataset.readme === 'string')
+    if (readmes[e.target.dataset.readme]) {
+      modalCode.innerHTML = readmes[e.target.dataset.readme]
+    } else {
+      const readmeResponse = await fetch(e.target.dataset.readme)
+      let readme = await readmeResponse.text()
+      readmes[modalTemplate.dataset.readme] = readme
+      modalCode.innerHTML = readme
+    }
+
+  })
+
+  div.addEventListener('mouseout', (e) => {
+
+    modalTemplate.style = 'display: none;'
+
+
+  })
 })
 
